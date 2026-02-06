26e162133fbd60991cf327ea8618baefeb7e6548
Mies VanBeek <vanbeekmies@gmail.com>
2026-02-06 13:36:45 -0700

    Added ICC4.c

--- ICC4.c
+++ ICC4.c
@@ -1,3 +1,31 @@
 //
 // Created by Mies VanBeek on 2026-02-06.
 //
+#include <stdio.h>
+
+int main(void) {
+    int width = 0, repetitions = 0;
+    char line[width+2];
+
+    scanf(" %d %d ", &width, &repetitions);
+
+    for (int i = 0; i <= width; i++) {
+        line[i] = ' ';
+    }
+    line[width+1] = '|';
+
+    for (int i = 0; i < repetitions; i++) {
+        for (int j = 0; j < width; j++) {
+            line[j] = 'o';
+            printf("%s\n", line);
+            line[j] = ' ';
+        }
+        for (int j = width; j >= 0; j--) {
+            line[j] = 'o';
+            printf("%s\n", line);
+            line[j] = ' ';
+        }
+    }
+
+    return 0;
+}
